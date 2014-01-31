466075ef64ee8380064005fd0be775ce769441f8
Joshua Mensah <jpkmensah@gmail.com>
2014-01-31 01:03:40 -0800

    Split training set into training and cross validation sets

--- identify_digits.py
+++ identify_digits.py
@@ -8,10 +8,30 @@
 # Copyright:   (c) Joshua Mensah 2014
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+import numpy
 
 def main():
-    print "Let's identify some digits!"
+    a = numpy.loadtxt('train_m.csv', delimiter=',')
     pass
+
+def split_train_cv():
+    print "Splitting original test set into test and cross validation"
+    f = open('train.csv')
+    i = 0
+    r = f.readline()
+    testfile = open('train_m.csv', 'w+')
+    cvfile = open('cv_m.csv', 'w+')
+    while r != "":
+        r = f.readline()
+        if i < 30000:
+            testfile.write(r)
+        else:
+            cvfile.write(r)
+        i += 1
+    # a = numpy.loadtxt('test.csv')
+    f.close()
+    testfile.close()
+    cvfile.close()
 
 if __name__ == '__main__':
     main()
