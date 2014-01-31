59991c035d196712357a02f69bf767e7fde9f863
Joshua Mensah <jpkmensah@gmail.com>
2014-01-30 23:07:26 -0800

    Created identify_digits.py

---
+++ identify_digits.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        identify_digits.py
+# Purpose:
+#
+# Author:      Joshua Mensah
+#
+# Created:     30/01/2014
+# Copyright:   (c) Joshua Mensah 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print "Let's identify some digits!"
+    pass
+
+if __name__ == '__main__':
+    main()
