90ae3a47e28c4045c7f36a652acda702565707ad
Joshua Mensah <jpkmensah@gmail.com>
2014-01-31 20:10:24 -0800

    SVM now uses polynomial kernel instead of rbf

--- identify_digits.py
+++ identify_digits.py
@@ -9,9 +9,46 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import numpy
+import scipy
+import pylab as pl
+import matplotlib.pyplot as plt
+from sklearn import svm, metrics
+import cPickle as pickle
 
 def main():
-    a = numpy.loadtxt('train_m.csv', delimiter=',')
+    a = numpy.loadtxt('train_m.csv', delimiter=',', skiprows=1)
+    # first column represents the label for the values
+    labels = a[:,0]
+    n_examples = len(labels)
+    values = scipy.delete(a,0,1)
+
+    classifier = svm.SVC(C=1, gamma=0.001, degree=2, kernel='poly')
+    classifier.fit(values, labels)
+
+    b = numpy.loadtxt('cv_m.csv', delimiter=',', skiprows=1)
+    cv_labels = b[:,0]
+    cv_values = scipy.delete(b,0,1)
+
+    expected = cv_labels
+    predicted = classifier.predict(cv_values)
+
+    # pickle.dump(classifier, open("classifier.p", "wb"))
+
+    '''
+    for index, (image, expected, prediction) in enumerate(zip(cv_values, expected, predicted)[:4]):
+        # cv_square = cv_values[1].reshape((28,28))
+        pl.subplot(1,3, index + 1)
+        cv_square = image.reshape((28,28))
+        pl.imshow(cv_square, cmap=pl.cm.gray_r, interpolation='nearest')
+        # pl.title('Training: %i Prediction: %i' % (cv_labels[1], predicted[1]))
+        pl.title('T: %i P: %i' % (expected, prediction))
+
+    pl.show()
+    '''
+
+    print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.classification_report(expected,predicted)))
+    print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+
     pass
 
 def split_train_cv():
