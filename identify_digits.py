dd5b9ebf1fa89583658d6d73c6376711fd5317bf
Joshua Mensah <jpkmensah@gmail.com>
2014-02-01 02:04:35 -0800

    Achieve 97.78% accuracy on submission

--- identify_digits.py
+++ identify_digits.py
@@ -1,22 +1,21 @@
 #-------------------------------------------------------------------------------
 # Name:        identify_digits.py
-# Purpose:
+# Purpose:     Identify a set of handwritten digits using an SVM
 #
 # Author:      Joshua Mensah
 #
 # Created:     30/01/2014
 # Copyright:   (c) Joshua Mensah 2014
-# Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import numpy
 import scipy
 import pylab as pl
 import matplotlib.pyplot as plt
 from sklearn import svm, metrics
-import cPickle as pickle
+import os
 
 def main():
-    a = numpy.loadtxt('train_m.csv', delimiter=',', skiprows=1)
+    a = numpy.loadtxt('train.csv', delimiter=',', skiprows=1)
     # first column represents the label for the values
     labels = a[:,0]
     n_examples = len(labels)
@@ -25,50 +24,70 @@ def main():
     classifier = svm.SVC(C=1, gamma=0.001, degree=2, kernel='poly')
     classifier.fit(values, labels)
 
-    b = numpy.loadtxt('cv_m.csv', delimiter=',', skiprows=1)
-    cv_labels = b[:,0]
-    cv_values = scipy.delete(b,0,1)
+    test_values = numpy.loadtxt('test.csv', delimiter=',', skiprows=1)
 
-    expected = cv_labels
-    predicted = classifier.predict(cv_values)
+    predicted = classifier.predict(test_values)
 
-    # pickle.dump(classifier, open("classifier.p", "wb"))
-
-    '''
-    for index, (image, expected, prediction) in enumerate(zip(cv_values, expected, predicted)[:4]):
-        # cv_square = cv_values[1].reshape((28,28))
-        pl.subplot(1,3, index + 1)
-        cv_square = image.reshape((28,28))
-        pl.imshow(cv_square, cmap=pl.cm.gray_r, interpolation='nearest')
-        # pl.title('Training: %i Prediction: %i' % (cv_labels[1], predicted[1]))
-        pl.title('T: %i P: %i' % (expected, prediction))
-
-    pl.show()
-    '''
-
-    print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.classification_report(expected,predicted)))
-    print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
-
+    predictions_file = open('predictions.csv', 'w')
+    img_id = 1
+    predictions_file.write('ImageId,Label\n')
+    for guess in predicted:
+        output_line = "%d,%d\n" % (img_id, guess)
+        predictions_file.write(output_line)
+        img_id += 1
+    predictions_file.close()
     pass
 
-def split_train_cv():
-    print "Splitting original test set into test and cross validation"
-    f = open('train.csv')
-    i = 0
-    r = f.readline()
-    testfile = open('train_m.csv', 'w+')
-    cvfile = open('cv_m.csv', 'w+')
-    while r != "":
-        r = f.readline()
-        if i < 30000:
-            testfile.write(r)
+def split_train_cv(training_filename):
+    # Split the training set into training and cross validation sets.
+    # By default training set will be 75% and cross validation 25% of the
+    # original training file by lines. Removes header file.
+    training_file = open(training_filename, 'r')
+    current_line = training_file.readline()
+    line_count = 0
+    while current_line != "":
+        current_line = training_file.readline()
+        line_count += 1
+    training_file.seek(0)
+    current_line = training_file.readline()
+    split_line = line_count * .75
+    (training_filename_short, training_filename_ext) = \
+            os.path.splitext(training_filename)
+    line_number = 0
+    new_training_file = open(training_filename_short + \
+            '_train' + training_filename_ext, 'w')
+    new_cv_file = open(training_filename_short + \
+            '_cv' + training_filename_ext, 'w')
+    while current_line != "":
+        current_line = training_file.readline()
+        if line_number < split_line:
+            new_training_file.write(current_line)
         else:
-            cvfile.write(r)
-        i += 1
-    # a = numpy.loadtxt('test.csv')
-    f.close()
-    testfile.close()
-    cvfile.close()
+            new_cv_file.write(current_line)
+        line_number += 1
+    training_file.close()
+    new_training_file.close()
+    new_cv_file.close()
+
+def test_on_cv(training_filename, cv_filename):
+    training_set = numpy.loadtxt(training_filename, delimiter=',')
+    training_labels = training_set[:,0]
+    n_examples = len(labels)
+    training_values = scipy.delete(training_set,0,1)
+
+    classifier = svm.SVC(C=1, gamma=0.001, degree=2, kernel='poly')
+    classifier.fit(values, labels)
+
+    cv_set = numpy.loadtxt(cv_filename, delimiter=',')
+    cv_labels = cv_set[:,0]
+    cv_values = scipy.delete(cv_set,0,1)
+
+    cv_predicted = classifier.predict(cv_values)
+
+    print("Classification report for classifier %s:\n%s\n" % \
+            (classifier, metrics.classification_report(cv_labels,predicted)))
+    print("Confusion matrix:\n%s" % \
+            metrics.confusion_matrix(cv_labels, predicted))
 
 if __name__ == '__main__':
     main()
